e774997f919372df2cf8bba67e229dc018871ebf
Lucas Vázquez <lucasvazquezarias@gmail.com>
2025-11-19 00:32:36 +0100

    Exercise 28

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,6 +2,17 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        int dividendo = 3;
+        int divisor = 0;
+        try{
+            int resultado = dividendo/divisor;
+            System.out.println("pintar resuñtado"+ resultado);
+        }catch(ArithmeticException e){
+            System.out.println("no puedes dividir por 0");
+        }finally {
+            System.out.println("Acabo el programa");
+
+        }
 
     }
 }
